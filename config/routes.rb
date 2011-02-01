e100b043e07cbba1611fb6f50f53868c54df7c5e
codester <elad.hemar@gmail.com>
2011-02-01 14:41:41 +0200

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Bestapp::Application.routes.draw do
+  # get "home/index"
+
   resources :users
+  
+  #...
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  root :to => "home#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
